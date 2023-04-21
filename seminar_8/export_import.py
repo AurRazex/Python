4f3355702384d263d9b57fc27b0bf8003c5eeff1
AurRazex <98745871+AurRazex@users.noreply.github.com>
2023-04-21 12:54:07 +0300

    1

--- seminar_8/export_import.py
+++ seminar_8/export_import.py
@@ -3,14 +3,14 @@ def get_file_name() -> str:
 
 def get_batch_data(name_file: str) -> list:
     lst = []
-    with open('data01.txt', 'r', encoding = 'utf-8') as file:
+    with open(input(), 'r', encoding = 'utf-8') as file:
         lst.extend(list(line.split('#')) for line in file)
     return lst
 
 
 
 def record_data(name_file, data):
-    with open ('data01.txt', 'w', encoding = 'utf-8') as file:
+    with open (input(), 'w', encoding = 'utf-8') as file:
         for el in data:
             file.write(f'{el[0]};{el[1]};{el[2]};{el[3]}\n')
 
